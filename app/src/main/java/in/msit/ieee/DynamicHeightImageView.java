1f0e97529c7bc51916fa671e3233eaeafaf18346
Shivam Rastogi <shivam.ras304@gmail.com>
2017-08-05 23:44:37 +0530

    Added 3 page layouts

---
+++ app/src/main/java/in/msit/ieee/DynamicHeightImageView.java
@@ -0,0 +1,38 @@
+package in.msit.ieee;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by shivam on 8/5/17.
+ */
+
+public class DynamicHeightImageView extends android.support.v7.widget.AppCompatImageView {
+
+    private float mAspectRatio = 1;
+
+    public DynamicHeightImageView(Context context) {
+        super(context);
+    }
+
+    public DynamicHeightImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DynamicHeightImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setAspectRatio(float aspectRatio) {
+        mAspectRatio = aspectRatio;
+        requestLayout();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int measuredWidth = getMeasuredWidth();
+        setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+    }
+}
